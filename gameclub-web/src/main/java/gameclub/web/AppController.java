31bf7c6ee3644beffa710f61ebed612c616daae7
Csonka Bálint <csonkabalintka@gmail.com>
2022-04-22 00:05:15 +0200

    dunno atiroma a application osztályt

--- gameclub-web/src/main/java/gameclub/web/AppController.java
+++ gameclub-web/src/main/java/gameclub/web/AppController.java
@@ -1,8 +1,18 @@
 package gameclub.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 public class AppController {
-    
+
+    @GetMapping(value = "/")
+    public String test(){
+        return "test";
+    }
+
 }
