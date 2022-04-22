a6ebe63434c7b108195d7363a7ad31cfdc373b44
Csonka Bálint <csonkabalintka@gmail.com>
2022-04-22 17:23:30 +0200

    added first

---
+++ gameclub-web/src/main/java/gameclub/model/TestModel.java
@@ -0,0 +1,6 @@
+package gameclub.model;
+
+import org.springframework.ui.Model;
+
+/*public class TestModel implements Model {
+}*/
--- gameclub-web/src/main/java/gameclub/web/AppController.java
+++ gameclub-web/src/main/java/gameclub/web/AppController.java
@@ -1,17 +1,16 @@
 package gameclub.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class AppController {
 
     @GetMapping(value = "/")
-    public String test(){
+    public String test(Model model){
+        model.addAttribute("name", "name");
         return "test";
     }
 
